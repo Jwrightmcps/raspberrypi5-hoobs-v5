7cbf5655ac6b32421e9aaee20bbac89066e27f63
Jwrightmcps <jwrightmcps@icloud.com>
2025-02-17 09:53:23 -0600

    Update setup-hoobs-for-raspi-5.sh

--- setup-hoobs-for-raspi-5.sh
+++ setup-hoobs-for-raspi-5.sh
@@ -6,25 +6,6 @@ sudo rpi-eeprom-update
 sudo raspi-config
 sudo apt update && sudo apt upgrade -y
 
-echo # change 1 System Options>S5 Boot / Auto Login Select boot into desktop or to command line>B4 Desktop Autologon Desktop GUI, automatically logged in as 'hoobs' user
-echo # change 1 System Options>S6 Splash Screen>No
-echo # change 2 Display Options>D6 Onscreen Keyboard Enable on-screen keyboard>S1 Always On On-screen keyboard always enabled
-echo # change 6 Advanced Options> A4 Boot Order> B2 NVMe/USB boot
-echo # change 6 Advanced Options> A5 bootloader Version> Latest>Yes
-echo # change 6 Advanced Options> A6 Wayland>W2 Wayfire window manager with Wayland backend
-echo # change 6 Advanced Options> A8 PCIe Speed>Yes
-
-echo # Appearance Settings>Defaults>For large screens>Set Defaults
-echo # Appearance Settings>System>Theme>Dark
-echo # Appearance Settings>Desktop>Layout>No Image
-echo # Appearance Settings>Desktop>Layout>Colour>Black
-echo # Appearance Settings>Desktop>Layout>Colour>Black
-echo # Appearance Settings>Desktop>Layout>Documents>Disable
-echo # Appearance Settings>Desktop>Layout>Wastebasket>Disable
-echo # Appearance Settings>Desktop>Layout>External Disks>Disable
-
-echo # Configure clock (%I%M%p)
-
 sudo apt install -y interception-tools interception-tools-compat cmake
 cd ~
 git clone https://gitlab.com/interception/linux/plugins/hideaway.git
@@ -45,8 +26,10 @@ sudo apt install hoobsd hoobs-cli -y
 sudo hbs install
 
 wget https://raw.githubusercontent.com/Jwrightmcps/raspberrypi5-hoobs-v5/refs/heads/main/hoobs-ui.desktop
-wget https://raw.githubusercontent.com/Jwrightmcps/raspberrypi5-hoobs-v5/refs/heads/main/disable-hoobs.kiosk.sh
+wget https://raw.githubusercontent.com/Jwrightmcps/raspberrypi5-hoobs-v5/refs/heads/main/disable-hoobs-kiosk.sh
 wget https://raw.githubusercontent.com/Jwrightmcps/raspberrypi5-hoobs-v5/refs/heads/main/enable-hoobs-kiosk.sh
+wget https://raw.githubusercontent.com/Jwrightmcps/raspberrypi5-hoobs-v5/refs/heads/main/tools.txt
+chmod +x disable-hoobs-kiosk.sh
+chmod +x enable-hoobs-kiosk.sh
 mkdir ~/.config/autostart
 cp ~/hoobs-ui.desktop ~/.config/autostart/hoobs-ui.desktop
-
