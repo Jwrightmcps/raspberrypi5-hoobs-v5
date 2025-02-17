36764acec1a7c4780ba6566359b383dbdbbf65d7
Jwrightmcps <jwrightmcps@icloud.com>
2025-02-16 21:17:28 -0600

    Added initial release files
    Initial release files

---
+++ setup-hoobs-for-raspi-5.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Custom setup for RaspberryPi 5 Hoobs Server with Desktop support
+
+sudo apt update && sudo apt upgrade -y
+sudo rpi-eeprom-update
+sudo raspi-config
+
+echo # change 1 System Options>S5 Boot / Auto Login Select boot into desktop or to command line>B4 Desktop Autologon Desktop GUI, automatically logged in as 'hoobs' user
+echo # change 1 System Options>S6 Splash Screen>No
+echo # change 2 Display Options>D6 Onscreen Keyboard Enable on-screen keyboard>S1 Always On On-screen keyboard always enabled
+echo # change 6 Advanced Options> A4 Boot Order> B2 NVMe/USB boot
+echo # change 6 Advanced Options> A5 bootloader Version> Latest>Yes
+echo # change 6 Advanced Options> A6 Wayland>W2 Wayfire window manager with Wayland backend
+echo # change 6 Advanced Options> A8 PCIe Speed>Yes
+
+echo # Appearance Settings>Defaults>For large screens>Set Defaults
+echo # Appearance Settings>System>Theme>Dark
+echo # Appearance Settings>Desktop>Layout>No Image
+echo # Appearance Settings>Desktop>Layout>Colour>Black
+echo # Appearance Settings>Desktop>Layout>Colour>Black
+echo # Appearance Settings>Desktop>Layout>Documents>Disable
+echo # Appearance Settings>Desktop>Layout>Wastebasket>Disable
+echo # Appearance Settings>Desktop>Layout>External Disks>Disable
+
+echo # Configure clock (%I%M%p)
+
+sudo apt install -y interception-tools interception-tools-compat cmake
+cd ~
+git clone https://gitlab.com/interception/linux/plugins/hideaway.git
+cd hideaway
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build
+sudo cp ~/hideaway/build/hideaway /usr/bin
+sudo chmod +x /usr/bin/hideaway
+cd ~
+wget https://raw.githubusercontent.com/ugotapi/wayland-pagepi/main/config.yaml
+#sudo nano ~/config.yaml
+sudo cp ~/config.yaml /etc/interception/udevmon.d/config.yaml
+sudo nano /etc/interception/udevmon.d/config.yaml
+sudo systemctl restart udevmon
+
+wget -qO- https://dl.hoobs.org/stable | sudo bash -
+sudo apt install hoobsd hoobs-cli -y
+sudo hbs install
+
+wget https://raw.github.com/Jwrightmcps/raspberrypi5-hoobs-v5/hoobs-ui.desktop
+mkdir ~/.config/autostart
+cp ~/hoobs-ui.desktop ~/.config/autostart/hoobs-ui.desktop
+nano ~/.config/autostart/hoobs-ui.desktop
