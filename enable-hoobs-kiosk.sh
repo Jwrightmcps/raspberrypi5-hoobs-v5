1a8c9a750856f15620e62106d1333abceb782d93
Jwrightmcps <jwrightmcps@icloud.com>
2025-02-18 18:52:27 -0600

    Update enable-hoobs-kiosk.sh

--- enable-hoobs-kiosk.sh
+++ enable-hoobs-kiosk.sh
@@ -3,3 +3,4 @@
 
 sed -i 's/Exec=chromium-browser --wayland-text-input-version=3 --enable-wayland-ime --enable-virtual-keyboard/#Exec=chromium-browser --wayland-text-input-version=3 --enable-wayland-ime --enable-virtual-keyboard/' ~/.config/autostart/hoobs-ui.desktop
 sed -i 's/#Exec=chromium-browser --noerrdialogs --disable-infobars --kiosk/Exec=chromium-browser --noerrdialogs --disable-infobars --kiosk/' ~/.config/autostart/hoobs-ui.desktop
+sudo reboot
