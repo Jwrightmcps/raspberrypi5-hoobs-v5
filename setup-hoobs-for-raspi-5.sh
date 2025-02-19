6fa85f9fab63b47859c92c59fe82af5de664ef2f
Jwrightmcps <jwrightmcps@icloud.com>
2025-02-18 21:53:35 -0600

    Update setup-hoobs-for-raspi-5.sh

--- setup-hoobs-for-raspi-5.sh
+++ setup-hoobs-for-raspi-5.sh
@@ -2,15 +2,16 @@
 # Custom setup for RaspberryPi 5 Hoobs Server with Desktop support
  #wget https://raw.githubusercontent.com/Jwrightmcps/raspberrypi5-hoobs-v5/refs/heads/main/setup-hoobs-for-raspi-5.sh && chmod +x setup-hoobs-for-raspi-5.sh && ./setup-hoobs-for-raspi-5.sh 2>&1 | tee setup-hoobs-for-raspi-5.log
 
+# Set the locale to en-US if not already set
 if [ "$(grep LANG=en_US /etc/default/locale)" = "LANG=en_US" ];then
 	exit 0
     else
 	sudo sh -c 'echo "LANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8\nLC_ALL=en_US.UTF-8" > /etc/default/locale'
 fi
-
+# Update the OS
 sudo apt update && sudo apt upgrade -y
+# Update the firmware
 sudo rpi-eeprom-update
-
 # Get the Raspberry Pi Board Type (4 = pi4, 5 = pi5, etc.)
 sudo raspi-config nonint get_pi_type 
 # Set locale to en-US
@@ -35,9 +36,11 @@ sudo raspi-config nonint do_wayland W2
 sudo raspi-config nonint do_boot_rom E1
 # Update the keyboard
 sudo raspi-config nonint update_wayfire_keyboard
-
+# Install interception tools
 sudo apt install -y interception-tools interception-tools-compat cmake
+# Change into home directory
 cd ~
+# Clone the hideaway repo and build
 git clone https://gitlab.com/interception/linux/plugins/hideaway.git
 cd hideaway
 cmake -B build -DCMAKE_BUILD_TYPE=Release
@@ -48,23 +51,25 @@ cd ~
 wget https://raw.githubusercontent.com/ugotapi/wayland-pagepi/main/config.yaml
 sed -i 's/hideaway 4/hideaway 5/' ~/config.yaml
 sudo cp ~/config.yaml /etc/interception/udevmon.d/config.yaml
-
+# Set the repo for Hoobs
 wget -qO- https://dl.hoobs.org/stable | sudo bash -
+# Install hoobs server and command line tools
 sudo apt install hoobsd hoobs-cli -y
+# Install Hoobs Hub
 sudo hbs install -p 80
-
+# Create autostart directory
 if [ ! -d ~/.config/autostart ]; then
 mkdir ~/.config/autostart
 fi
-
+# Create pcmanfm directory
 if [ ! -d ~/.config/pcmanfm ]; then
 mkdir ~/.config/pcmanfm
 fi
-
+# Create pcmanfm/LXDE-pi directory
 if [ ! -d ~/.config/pcmanfm/LXDE-pi ]; then
 mkdir ~/.config/pcmanfm/LXDE-pi
 fi
-
+# Get supporting files
 wget https://raw.githubusercontent.com/Jwrightmcps/raspberrypi5-hoobs-v5/refs/heads/main/hoobs-ui.desktop -O ~/.config/autostart/hoobs-ui.desktop
 wget https://raw.githubusercontent.com/Jwrightmcps/raspberrypi5-hoobs-v5/refs/heads/main/wf-panel-pi.ini -O ~/.config/wf-panel-pi.ini
 wget https://raw.githubusercontent.com/Jwrightmcps/raspberrypi5-hoobs-v5/refs/heads/main/desktop-items-HDMI-A-1.conf -O ~/.config/pcmanfm/LXDE-pi/desktop-items-HDMI-A-1.conf
@@ -77,7 +82,7 @@ chmod +x enable-hoobs-kiosk.sh
 chmod +x launch-chrome-on-screen-keyboard.sh
 tar -xf on-screen-keyboard-ext.tar.gz
 mv gkiknnlmdgcmhmncldcmmnhhdiakielc .gkiknnlmdgcmhmncldcmmnhhdiakielc
-
+# Fix-up for screen blanking
 if grep -Fxq "[idle]" ~/.config/wayfire.ini; then
   sed -i 's/dpms_timeout=600/dpms_timeout=180/' ~/.config/wayfire.ini
   sed  '/\[idle\]/a disable_on_fullscreen = false' ~/.config/wayfire.ini
@@ -86,14 +91,13 @@ printf "%s\n" "[idle]" >> ~/.config/wayfire.ini
 printf "%s\n" "disable_on_fullscreen = false" >> ~/.config/wayfire.ini
 printf "%s\n" "dpms_timeout=180" >> ~/.config/wayfire.ini
 fi
-
+# Clean-up
 rm config.yaml
 rm setup-hoobs-for-raspi-5.sh
 rm on-screen-keyboard-ext.tar.gz
 rm -rf ~/hideaway/
-
-# Apply settings
+# Apply raspi settings
 sudo raspi-config nonint do_finish
 printf "%s\n" "Rebooting system for all changes to take effect"
-
+# Reboot system
 sudo reboot
