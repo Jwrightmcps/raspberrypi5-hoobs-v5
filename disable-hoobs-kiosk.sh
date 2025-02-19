658654687bd40d4befe3533fee9fbd7957e12b9d
Jwrightmcps <jwrightmcps@icloud.com>
2025-02-18 19:31:49 -0600

    Update disable-hoobs-kiosk.sh

--- disable-hoobs-kiosk.sh
+++ disable-hoobs-kiosk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Disable Hoobs Kiosk Mode
-sed -i 's/#Exec=chromium-browser --noerrdialogs --disable-infobars --start-fullscreen/Exec=chromium-browser --noerrdialogs --disable-infobars --start-fullscreen/' ~/.config/autostart/hoobs-ui.desktop
+sed -i 's/#Exec=chromium-browser --wayland-text-input-version=3 --enable-wayland-ime --enable-virtual-keyboard/Exec=chromium-browser --wayland-text-input-version=3 --enable-wayland-ime --enable-virtual-keyboard/' ~/.config/autostart/hoobs-ui.desktop
 sed -i 's/Exec=chromium-browser --noerrdialogs --disable-infobars --kiosk/#Exec=chromium-browser --noerrdialogs --disable-infobars --kiosk/' ~/.config/autostart/hoobs-ui.desktop
 sudo reboot
